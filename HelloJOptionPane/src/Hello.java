27f1db66a361bc121d7e76d756e68e1fc4d924a0
tkanchanawanchai6403 <tkanchanawanchai6403@an098073-ct107v.nvstu.edu>
2015-02-04 15:12:36 -0500

    Update to Hello World

--- HelloJOptionPane/src/Hello.java
+++ HelloJOptionPane/src/Hello.java
@@ -5,7 +5,8 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		JOptionPane.showMessageDialog(null,"Hello");
+		JOptionPane.showMessageDialog(null,"Hello World");
+		
 	}
 
 }
